f16dbe738dc9c340981775c28c83f4251cea07ee
Vinicius Borges <54821518+DetetiveVirtual@users.noreply.github.com>
2021-04-27 18:12:45 -0300

    coisas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const {uuid, isUuid} = require('uuidv4');
 
 const app = express();
 
 app.use(express.json())
+
+app.use(cors());
 
 app.use('/projects/:id', validateProjectId);
 
